0746a26b963dabc956714b1e916778088ae269d0
Aleksandr Pisetskij <aleksandr.pisetskij@gmail.com>
2026-09-06 17:51:01 +0300

    +++

---
+++ ConferenceHub/Data/DatabaseSeeder.cs
@@ -0,0 +1,109 @@
+﻿using ConferenceHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConferenceHub.Data;
+
+public static class DatabaseSeeder
+{
+    public static async Task SeedAsync(
+        ConferenceHubDbContext context,
+        CancellationToken cancellationToken = default)
+    {
+        if (await context.Halls.AnyAsync(cancellationToken))
+        {
+            return;
+        }
+
+        var projector = new Service
+        {
+            Name = "Проєктор",
+            Price = 500m
+        };
+
+        var wifi = new Service
+        {
+            Name = "Wi-Fi",
+            Price = 300m
+        };
+
+        var sound = new Service
+        {
+            Name = "Звук",
+            Price = 700m
+        };
+
+        context.Services.AddRange(projector, wifi, sound);
+
+        var hallA = new Hall
+        {
+            Name = "Зал А",
+            Capacity = 50,
+            BaseHourlyRate = 2000m
+        };
+
+        var hallB = new Hall
+        {
+            Name = "Зал B",
+            Capacity = 100,
+            BaseHourlyRate = 3500m
+        };
+
+        var hallC = new Hall
+        {
+            Name = "Зал C",
+            Capacity = 30,
+            BaseHourlyRate = 1500m
+        };
+
+        context.Halls.AddRange(hallA, hallB, hallC);
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        context.HallServices.AddRange(
+            new HallService
+            {
+                HallId = hallA.Id,
+                ServiceId = projector.Id
+            },
+            new HallService
+            {
+                HallId = hallA.Id,
+                ServiceId = wifi.Id
+            },
+            new HallService
+            {
+                HallId = hallA.Id,
+                ServiceId = sound.Id
+            },
+
+            new HallService
+            {
+                HallId = hallB.Id,
+                ServiceId = projector.Id
+            },
+            new HallService
+            {
+                HallId = hallB.Id,
+                ServiceId = wifi.Id
+            },
+            new HallService
+            {
+                HallId = hallB.Id,
+                ServiceId = sound.Id
+            },
+
+            new HallService
+            {
+                HallId = hallC.Id,
+                ServiceId = projector.Id
+            },
+            new HallService
+            {
+                HallId = hallC.Id,
+                ServiceId = wifi.Id
+            }
+        );
+
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
--- ConferenceHub/Program.cs
+++ ConferenceHub/Program.cs
@@ -3,16 +3,34 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var databaseProvider = builder.Configuration["DatabaseProvider"] ?? "SqlServer";
+
 // Add services to the container.
-builder.Services.AddDbContext<ConferenceHubDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("ConferenceHub")));
+if (databaseProvider.Equals("InMemory", StringComparison.OrdinalIgnoreCase))
+{
+    builder.Services.AddDbContext<ConferenceHubDbContext>(options =>
+        options.UseInMemoryDatabase("ConferenceHub"));
+}
+else
+{
+    builder.Services.AddDbContext<ConferenceHubDbContext>(options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("ConferenceHub")));
+}
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<ConferenceHubDbContext>();
+
+    await DatabaseSeeder.SeedAsync(dbContext);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
